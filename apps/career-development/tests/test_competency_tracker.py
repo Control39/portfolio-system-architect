1a79b797af8814217ef81daaf4901b7a9ccef022
﻿dsgfcrels@sourcecraft.dev=Екатерина Куделя <leadarchitect@yandex.ru>
2026-04-25 09:43:56 +0200

    fix(career-development): correct imports in tests

--- apps/career-development/tests/test_competency_tracker.py
+++ apps/career-development/tests/test_competency_tracker.py
@@ -1,6 +1,6 @@
 ﻿import unittest
-from src.core.competency_tracker import CompetencyTracker
-
+from apps.career_development.src.core.competency_tracker import CompetencyTracker
+from apps.career_development.src.core.models import UserProfile
 
 class TestCompetencyTracker(unittest.TestCase):
 
